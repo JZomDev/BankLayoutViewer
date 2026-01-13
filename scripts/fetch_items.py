d020e6247b7d0198404806a70bc39d501167d54a
JZomDev <jzomerlei@gmail.com>
2026-01-12 22:54:35 -0500

    Start cdn

---
+++ scripts/fetch_items.py
@@ -0,0 +1,172 @@
+"""
+    Script to generate an items.json of all the items on the OSRS Wiki, and downloads images for each item.
+    The JSON file is placed in ../src/lib/items.json.
+
+    The images are placed in ../cdn/items/. This directory is NOT included in the Next.js app bundle, and should
+    be deployed separately to our file storage solution.
+
+    Written for Python 3.9.
+"""
+import os
+import requests
+import json
+import urllib.parse
+
+FILE_NAME = '../cdn/json/items.json'
+WIKI_BASE = 'https://oldschool.runescape.wiki'
+API_BASE = WIKI_BASE + '/api.php'
+IMG_PATH = '../cdn/items/'
+
+BUCKET_API_FIELDS = [
+    'item_name',
+    'image',
+    'item_id',
+]
+
+ITEMS_TO_SKIP = [
+    'The dogsword',
+    'Drygore blowpipe',
+    'Amulet of the monarchs',
+    'Emperor ring',
+    'Devil\'s element',
+    'Nature\'s reprisal',
+    'Gloves of the damned',
+    'Crystal blessing',
+    'Sunlight spear',
+    'Sunlit bracers',
+    'Thunder khopesh',
+    'Thousand-dragon ward',
+    'Arcane grimoire',
+    'Wristbands of the arena',
+    'Wristbands of the arena (i)',
+    'Armadyl chainskirt (or)',
+    'Armadyl chestplate (or)',
+    'Armadyl helmet (or)',
+    'Dagon\'hai hat (or)',
+    'Dagon\'hai robe bottom (or)',
+    'Dagon\'hai robe top (or)',
+    'Dragon warhammer (or)',
+    'Centurion cuirass',
+    'Ruinous powers (item)',
+    'Battlehat',
+    'Zaryte bow'
+]
+
+def getEquipmentData():
+    equipment = []
+    offset = 0
+    fields_csv = ",".join(map(repr, BUCKET_API_FIELDS))
+    while True:
+        print('Fetching equipment info: ' + str(offset))
+        query = {
+            'action': 'bucket',
+            'format': 'json',
+            'query': 
+            (
+                f"bucket('infobox_item')"
+                f".select({fields_csv})"
+                f".limit(500).offset({offset})"
+                f".where('item_id', '!=', bucket.Null())"
+                f".join('infobox_bonuses', 'infobox_bonuses.page_name_sub', 'infobox_item.page_name_sub')"
+                f".orderBy('page_name_sub', 'asc').run()"
+            )
+        }
+
+        r = requests.get(API_BASE + '?' + urllib.parse.urlencode(query), headers={
+            'User-Agent': 'JZomDev Bank Tags Layout helper'
+        })
+
+        data = r.json()
+
+        if 'bucket' not in data:
+            print("No bucket data found!")
+            # No results?
+            break
+
+        equipment = equipment + data['bucket']
+
+        # Bucket's API doesn't tell you when there are more results, so we'll just have to guess
+        if len(data['bucket']) == 500:
+            offset += 500
+        else:
+            # If we are at the end of the results, break out of this loop
+            break
+
+    return equipment
+
+
+def main():
+    # Grab the equipment info using Bucket
+    wiki_data = getEquipmentData()
+
+    # Use an object rather than an array, so that we can't have duplicate items with the same page_name_sub
+    data = {}
+    required_imgs = []
+
+    # Loop over the equipment data from the wiki
+    for v in wiki_data:
+        if v['page_name_sub'] in data:
+            continue
+
+        print(f"Processing {v['page_name_sub']}")
+
+        try:
+            item_id = int(v.get('item_id')[0]) if v.get('item_id') else None
+        except ValueError:
+            # Item has an invalid ID, do not show it here as it's probably historical or something.
+            print("Skipping - invalid item ID (not an int)")
+            continue
+
+        equipment = {
+            'name': v['page_name'],
+            'id': item_id,
+            'image': '' if not v.get('image') else v.get('image')[-1].replace('File:', ''),
+        }
+    
+        if equipment['name'] in ITEMS_TO_SKIP:
+            continue
+        # Set the current equipment item to the calc's equipment list
+        data[v['page_name_sub']] = equipment
+
+        if not equipment['image'] == '':
+            required_imgs.append(equipment['image'])
+
+    new_data = list(data.values())
+
+    print('Total equipment: ' + str(len(new_data)))
+    new_data.sort(key=lambda d: d.get('name'))
+
+    with open(FILE_NAME, 'w') as f:
+        print('Saving to JSON at file: ' + FILE_NAME)
+        json.dump(new_data, f, ensure_ascii=False, indent=2)
+
+    success_img_dls = 0
+    failed_img_dls = 0
+    skipped_img_dls = 0
+    required_imgs = set(required_imgs)
+
+    # Fetch all the images from the wiki and store them for local serving
+    for idx, img in enumerate(required_imgs):
+        if os.path.isfile(IMG_PATH + img):
+            skipped_img_dls += 1
+            continue
+
+        print(f'({idx}/{len(required_imgs)}) Fetching image: {img}')
+        r = requests.get(WIKI_BASE + '/w/Special:Filepath/' + img, headers={
+            'User-Agent': 'JZomDev Bank Tags Layout helper'
+        })
+        if r.ok:
+            with open(IMG_PATH + img, 'wb') as f:
+                f.write(r.content)
+                print('Saved image: ' + img)
+                success_img_dls += 1
+        else:
+            print('Unable to save image: ' + img)
+            failed_img_dls += 1
+
+    print('Total images saved: ' + str(success_img_dls))
+    print('Total images skipped (already exists): ' + str(skipped_img_dls))
+    print('Total images failed to save: ' + str(failed_img_dls))
+
+
+main()
